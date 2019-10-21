fffe062e6a08317ef8ce43a8c592e8cd8a7e9817
bingoohuang <bingoo.huang@gmail.com>
2019-10-21 11:29:58 +0800

    comment out User/Group  in haproxy.service

--- centos7/haproxy-1.8.21/install.sh
+++ centos7/haproxy-1.8.21/install.sh
@@ -4,8 +4,8 @@ useradd -r haproxy -s /sbin/nologin
 cp haproxy /usr/sbin/
 cp haproxy.service /usr/lib/systemd/system/
 cp haproxy.cfg /etc/haproxy.cfg
-touch /run/haproxy.pid
-chown haproxy:haproxy /run/haproxy.pid
+# touch /run/haproxy.pid
+# chown haproxy:haproxy /run/haproxy.pid
 systemctl daemon-reload
 systemctl enable haproxy
 systemctl start haproxy
