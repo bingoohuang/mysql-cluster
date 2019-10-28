778517374da7101e80e129f3e2b38d5fac98ab85
bingoohuang <bingoo.huang@gmail.com>
2019-10-28 19:12:51 +0800

    Version: 1.5.0

--- centos7/mysql-community-server-5.7.27-1.el7.x86_64/install.sh
+++ centos7/mysql-community-server-5.7.27-1.el7.x86_64/install.sh
@@ -9,5 +9,5 @@ systemctl enable mysqld
 systemctl start mysqld
 grep 'temporary password' /var/lib/mysql/mysqld.log
 password=$(grep -oP 'temporary password(.*): \K(\S+)' /var/lib/mysql/mysqld.log)
-newpwd=$(grep -oP 'RootPassword\s*=\s*"\K[^"]+' ../mysqlclusterinit/config.toml)
+newpwd=$(grep -oP '\bPassword\s*=\s*"\K[^"]+' ../mysqlclusterinit/config.toml)
 mysqladmin --user=root --password="$password" password "$newpwd"
