e27072745a1e542bafc862a5d0879161076d4677
bingoohuang <bingoo.huang@gmail.com>
2019-11-01 19:06:15 +0800

    mysql mysqldump upxed

--- centos7/mysql-community-server-5.7.27-1.el7.x86_64/install.sh
+++ centos7/mysql-community-server-5.7.27-1.el7.x86_64/install.sh
@@ -6,6 +6,7 @@ rpm -ivh *.rpm --nodeps --force
 chown mysql:mysql /var/lib/mysql
 systemctl daemon-reload
 systemctl enable mysqld
+# systemctl is-enabled mysqld
 systemctl start mysqld
 grep 'temporary password' /var/lib/mysql/mysqld.log
 password=$(grep -oP 'temporary password(.*): \K(\S+)' /var/lib/mysql/mysqld.log)
